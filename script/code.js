ce2400d44ae9dabfd33124ebb6d441d37c8db9f7
LC1007 <131274987+LC1007@users.noreply.github.com>
2023-05-16 16:31:51 +0200

    Update code.js

--- script/code.js
+++ script/code.js
@@ -2,3 +2,16 @@ let subjects = ['Python', 'C++', 'VB.Net', 'JavaScript', 'Assembly']
 
 console.log(subjects.pop());
 console.log(subjects[2]);
+
+let x = Math.floor(subjects.length / 2)
+// console.log(x);
+if(x % 2 == 0){
+    console.log(subjects[x]);
+} else {
+    console.log(subjects.slice(x - 1, x +  1));
+}
+
+function findElement(arr, index){
+    return index < arr.length ? arr[index] : "Index out of range"
+}
+console.log(findElement(subjects, 7));
